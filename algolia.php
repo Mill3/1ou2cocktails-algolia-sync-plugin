ca0a09afae4dab9d995b6347f2393d06f347c17a
Antoine Girard <thetoine@gmail.com>
2021-01-25 16:31:10 -0500

    added all files

---
+++ algolia.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * GitHub Plugin URI:  Mill3/denise-pelletier-algolia-sync-plugin
+ * GitHub Plugin URI:  https://github.com/Mill3/denise-pelletier-algolia-sync-plugin
+ * Plugin Name: 1ou2Cocktails - Algolia Sync
+ * Description: Sync data from Wordpress to Algolia
+ * Version: 0.5.1
+ * Author Name: Mill3 Studio (Antoine Girard)
+ *
+ * @package CSTJ_Algolia_Sync
+ */
+
+namespace WpAlgolia;
+class Main {
+
+    public $algolia_client;
+
+    public $registered_post_types = array();
+
+    public function __construct($algolia_client) {
+        $this->algolia_client = $algolia_client;
+    }
+
+    public function run() {
+        return $this->register();
+    }
+
+    public function search() {
+        return null;
+    }
+
+    private function register() {
+        $this->registered_post_types['cocktail'] = new \WpAlgolia\Register\Cocktail('cocktail', ALGOLIA_PREFIX . 'content', $this->algolia_client);
+        // $this->registered_post_types['post'] = new \WpAlgolia\Register\Post('post', ALGOLIA_PREFIX . 'content', $this->algolia_client);
+        // $this->registered_post_types['show'] = new \WpAlgolia\Register\Show('show', ALGOLIA_PREFIX . 'content', $this->algolia_client);
+        // $this->registered_post_types['page'] = new \WpAlgolia\Register\Page('page', ALGOLIA_PREFIX . 'content', $this->algolia_client);
+        // $this->registered_post_types['notebook'] = new \WpAlgolia\Register\Notebook('notebook', ALGOLIA_PREFIX . 'content', $this->algolia_client);
+        // $this->registered_post_types['biography'] = new \WpAlgolia\Register\Biography('biography', ALGOLIA_PREFIX . 'content', $this->algolia_client);
+        // $this->registered_post_types['formation'] = new \WpAlgolia\Register\Formation('formation', ALGOLIA_PREFIX . 'content', $this->algolia_client);
+        // $this->registered_post_types['room'] = new \WpAlgolia\Register\Room('room', ALGOLIA_PREFIX . 'content', $this->algolia_client);
+        // $this->registered_post_types['show_date'] = new \WpAlgolia\Register\ShowDate('show_date', ALGOLIA_PREFIX . 'show_date', $this->algolia_client);
+    }
+
+}
+
+add_action(
+    'plugins_loaded',
+    function () {
+
+        if(!defined('ALGOLIA_APPLICATION_ID') || !defined('ALGOLIA_ADMIN_API_KEY')) {
+            // Unless we have access to the Algolia credentials, stop here.
+            return;
+        }
+
+        if(!defined('ALGOLIA_PREFIX')) {
+            define('ALGOLIA_PREFIX', 'prod_');
+        }
+
+        require_once __DIR__ . '/vendor/autoload.php';
+        require_once __DIR__ . '/inc/AlgoliaIndex.php';
+        require_once __DIR__ . '/inc/RegisterAbstract.php';
+        require_once __DIR__ . '/inc/RegisterInterface.php';
+
+        // available post types
+        require_once __DIR__ . '/post_types/Post.php';
+        require_once __DIR__ . '/post_types/Cocktail.php';
+
+        // client
+        $algoliaClient = \Algolia\AlgoliaSearch\SearchClient::create(ALGOLIA_APPLICATION_ID, ALGOLIA_ADMIN_API_KEY);
+
+        // instance with supported post types
+        $instance = new \WpAlgolia\Main($algoliaClient);
+
+        // run
+        $instance->run();
+
+        // WP CLI commands.
+        if (defined('WP_CLI') && WP_CLI && $instance) {
+            require_once __DIR__ . '/inc/Commands.php';
+            $commands = new \WpAlgolia\Commands($instance);
+            \WP_CLI::add_command('algolia', $commands);
+        }
+
+    }
+);
---
+++ post_types/Cocktail.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of WpAlgolia plugin.
+ * (c) Antoine Girard for Mill3 Studio <antoine@mill3.studio>
+ * @version 0.0.7
+ */
+
+namespace WpAlgolia\Register;
+
+use WpAlgolia\RegisterAbstract as WpAlgoliaRegisterAbstract;
+use WpAlgolia\RegisterInterface as WpAlgoliaRegisterInterface;
+
+class Cocktail extends WpAlgoliaRegisterAbstract implements WpAlgoliaRegisterInterface
+{
+    public $searchable_fields = array('post_title', 'content', 'post_thumbnail');
+
+    public $acf_fields = array('subtitle', 'ingredients', 'steps');
+
+    public $taxonomies = array('spirit', 'flavour', 'type', 'style', 'theme', 'taste', 'occasion', 'tool');
+
+    public function __construct($post_type, $index_name, $algolia_client)
+    {
+        $index_config = array(
+            'acf_fields'        => $this->acf_fields,
+            'taxonomies'        => $this->taxonomies,
+            'post_type'         => $post_type,
+            'hidden_flag_field' => 'search_hidden',
+            'config'            => array(
+                'searchableAttributes'  => $this->searchableAttributes(),
+                'customRanking'         => array('asc(post_title)'),
+                // 'queryLanguages'        => array('fr', 'en'),
+            ),
+            array(
+               'forwardToReplicas' => true,
+            ),
+        );
+
+        parent::__construct($post_type, $index_name, $algolia_client, $index_config);
+    }
+
+    public function searchableAttributes()
+    {
+        return array_merge($this->searchable_fields, $this->acf_fields, $this->taxonomies);
+    }
+
+    // implement any special data handling for post type here
+    public function extraFields($data, $post) {
+
+        // set permalink as formatted url value
+        // $link_to_permalink = get_field('link_to_permalink', $postID);
+        // if ($link_to_permalink) {
+        //     $permalink = get_permalink($postID);
+        //     $data['formated_url'] = "href='{$permalink}'";
+        // }
+        return $data;
+    }
+}
